2c56519fd8592cf728d452eb91cf53695eaef9db
jimin <jiminseo0307@gmail.com>
2026-06-22 21:34:01 +0900

    Initial Commit

---
+++ Source/Defense/Defense.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Defense : ModuleRules
+{
+	public Defense(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Defense",
+			"Defense/Variant_Platforming",
+			"Defense/Variant_Platforming/Animation",
+			"Defense/Variant_Combat",
+			"Defense/Variant_Combat/AI",
+			"Defense/Variant_Combat/Animation",
+			"Defense/Variant_Combat/Gameplay",
+			"Defense/Variant_Combat/Interfaces",
+			"Defense/Variant_Combat/UI",
+			"Defense/Variant_SideScrolling",
+			"Defense/Variant_SideScrolling/AI",
+			"Defense/Variant_SideScrolling/Gameplay",
+			"Defense/Variant_SideScrolling/Interfaces",
+			"Defense/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/Defense/Defense.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Defense.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Defense, "Defense" );
+
+DEFINE_LOG_CATEGORY(LogDefense)
