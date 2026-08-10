20f0d7d1432d873db75670df363f39baf6811ea0
jimin <jiminseo0307@gmail.com>
2026-08-10 12:53:11 +0900

    ADD : 디렉토리 정리 및 Save Data 정의

---
+++ Source/Defense/Private/Profile/ProfileSubsystem.cpp
@@ -0,0 +1,30 @@
+﻿#include "Profile/ProfileSubsystem.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Profile/ProfileSaveGame.h"
+
+namespace
+{
+	const FString ProfileSlotName = TEXT("Profile");
+	constexpr int32 ProfileUserIndex = 0;
+}
+
+void UProfileSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	if (UGameplayStatics::DoesSaveGameExist(ProfileSlotName, ProfileUserIndex))
+	{
+		CurrentProfile = Cast<UProfileSaveGame>(
+			UGameplayStatics::LoadGameFromSlot(
+				ProfileSlotName,
+				ProfileUserIndex));
+	}
+	
+	if (!CurrentProfile)
+	{
+		CurrentProfile = Cast<UProfileSaveGame>(
+			UGameplayStatics::CreateSaveGameObject(
+				UProfileSaveGame::StaticClass()));
+	}
+}
---
+++ Source/Defense/Public/Profile/ProfileSubsystem.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ProfileSubsystem.generated.h"
+
+class UProfileSaveGame;
+
+UCLASS()
+class DEFENSE_API UProfileSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+	const UProfileSaveGame* GetCurrentProfile() const { return CurrentProfile; }
+	
+private:
+	UPROPERTY(Transient)
+	TObjectPtr<UProfileSaveGame> CurrentProfile;
+};
